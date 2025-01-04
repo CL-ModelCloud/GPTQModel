1f3cee575cde081a98a51506bb5d26b0c19423aa
CL-ModelCloud <cl@modelcloud.ai>
2025-01-04 10:29:09 +0800

    update test model path

--- tests/models/test_qwen2_5.py
+++ tests/models/test_qwen2_5.py
@@ -3,7 +3,7 @@ from model_test import ModelTest
 
 class TestQwen2_5(ModelTest):
     # NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-0.5B-Instruct"
-    NATIVE_MODEL_ID = "/root/projects/fanfiction-go/python/ai/train/config_file/config_qwen_qwq_32b_preview.ini"
+    NATIVE_MODEL_ID = "/monster/data/model/QwQ-32B-Preview"
     QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.2
     NATIVE_ARC_CHALLENGE_ACC = 0.2739
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3055
